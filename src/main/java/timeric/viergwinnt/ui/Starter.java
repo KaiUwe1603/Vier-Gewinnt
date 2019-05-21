e1db4d4507c3e7225d351f45c3ef4e12b8edc47e
timeric <timeric@famAlb-PC>
2019-05-21 20:01:22 +0200

    create starter class

---
+++ src/main/java/timeric/viergwinnt/ui/Starter.java
@@ -0,0 +1,11 @@
+package timeric.viergwinnt.ui;
+
+public class Starter {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hallo World");
+		
+	}
+
+}
