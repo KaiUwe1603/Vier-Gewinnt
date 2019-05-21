587bbef25f4b8ff5b4eb91c8101e21211151c53d
timeric <timeric@famAlb-PC>
2019-05-21 20:15:24 +0200

    making first step to start game

--- src/main/java/timeric/viergwinnt/ui/Starter.java
+++ src/main/java/timeric/viergwinnt/ui/Starter.java
@@ -1,11 +1,17 @@
 package timeric.viergwinnt.ui;
 
+import javax.swing.JFrame;
+
 public class Starter {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hallo World");
+		JFrame mainFrame = new JFrame("Vier Gewinnt");
+		mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		mainFrame.setSize(1200, 800);
+		mainFrame.setLocationByPlatform(true);
 		
+		
+		mainFrame.setVisible(true);
 	}
 
 }
