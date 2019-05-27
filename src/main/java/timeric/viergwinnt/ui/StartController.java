05e97b3cc75d0117a6dedfe3d8e835bb5ee56682
timeric <timeric@famAlb-PC>
2019-05-27 21:16:58 +0200

    Panel und Buttons

--- src/main/java/timeric/viergwinnt/ui/StartController.java
+++ src/main/java/timeric/viergwinnt/ui/StartController.java
@@ -1,19 +1,39 @@
 package timeric.viergwinnt.ui;
 
+import java.awt.BorderLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+
+import javax.swing.JButton;
 import javax.swing.JComponent;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public final class StartController {
 
 	public JComponent getViewComponent() {
-		JPanel viewComponent = new JPanel();
+		JPanel viewComponent = new JPanel(new BorderLayout());
 		
-		JLabel hallo = new JLabel("Hallo Welt!");
-		JLabel by = new JLabel("Tschüss!");
+		JPanel centerPane = new JPanel(new GridBagLayout());
 
-		viewComponent.add(hallo);
-		viewComponent.add(by);
+		GridBagConstraints gbc = new GridBagConstraints();
+		gbc.gridx = 0;
+		gbc.gridy = 0;
+		gbc.fill = GridBagConstraints.HORIZONTAL;
+		gbc.insets = new Insets(20, 0, 20, 0);
+
+		centerPane.add(new JButton("Einfaches Spiel"), gbc);
+
+		gbc.gridy = 1;
+		centerPane.add(new JButton("Großes Spielfeld"), gbc);
+
+		gbc.gridy = 2;
+		centerPane.add(new JButton("3 Spieler (großes Spielfeld)"), gbc);
+
+		gbc.gridy = 3;
+		centerPane.add(new JButton("Spielregeln"), gbc);
+
+		viewComponent.add(centerPane, BorderLayout.CENTER);
 		return viewComponent;
 		
 	}
