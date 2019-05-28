726491cb7cbf9be1546eec5290ef2b7c11817bd8
timeric <timeric@famAlb-PC>
2019-05-28 20:59:11 +0200

    Regelfenster

--- src/main/java/timeric/viergwinnt/ui/StartController.java
+++ src/main/java/timeric/viergwinnt/ui/StartController.java
@@ -4,9 +4,11 @@ import java.awt.BorderLayout;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JComponent;
+import javax.swing.JDialog;
 import javax.swing.JPanel;
 
 public final class StartController {
@@ -22,20 +24,35 @@ public final class StartController {
 		gbc.fill = GridBagConstraints.HORIZONTAL;
 		gbc.insets = new Insets(20, 0, 20, 0);
 
-		centerPane.add(new JButton("Einfaches Spiel"), gbc);
+		centerPane.add(createButton("Einfaches Spiel", null), gbc);
 
 		gbc.gridy = 1;
-		centerPane.add(new JButton("Großes Spielfeld"), gbc);
+		centerPane.add(createButton("Großes Spielfeld", null), gbc);
 
 		gbc.gridy = 2;
-		centerPane.add(new JButton("3 Spieler (großes Spielfeld)"), gbc);
+		centerPane.add(createButton("3 Spieler (großes Spielfeld)", null), gbc);
 
 		gbc.gridy = 3;
-		centerPane.add(new JButton("Spielregeln"), gbc);
+		centerPane.add(createButton("Spielregeln", e -> openRuleDialog()), gbc);
 
 		viewComponent.add(centerPane, BorderLayout.CENTER);
 		return viewComponent;
 		
+	}
+
+	private JButton createButton(String label, ActionListener action) {
+		JButton button = new JButton(label);
+		button.addActionListener(action);
+		return button;
+	}
+
+	private void openRuleDialog() {
+		JDialog rulesDialog = new JDialog();
+		rulesDialog.setTitle("Spielregeln");
+		rulesDialog.setSize(1000, 700);
+		rulesDialog.setLocationByPlatform(true);
+
+		rulesDialog.setVisible(true);
 	}
 
 }
